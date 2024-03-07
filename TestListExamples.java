f62d60f5e6bf9a67a3fff3c389b2fdabd799768e
bl-CSE15L <156377155+bl-CSE15L@users.noreply.github.com>
2024-03-07 14:27:24 -0800

    done

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,21 +1,45 @@
 import static org.junit.Assert.*;
-import org.junit.*;
-import java.util.Arrays;
+
+import java.util.ArrayList;
 import java.util.List;
 
-class IsMoon implements StringChecker {
-  public boolean checkString(String s) {
-    return s.equalsIgnoreCase("moon");
-  }
-}
+import org.junit.*;
 
-public class TestListExamples {
-  @Test(timeout = 500)
-  public void testMergeRightEnd() {
-    List<String> left = Arrays.asList("a", "b", "c");
-    List<String> right = Arrays.asList("a", "d");
-    List<String> merged = ListExamples.merge(left, right);
-    List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
-    assertEquals(expected, merged);
-  }
+public class ListTests {
+    
+    List<String> input1 = new ArrayList<>();
+    List<String> input2 = new ArrayList<>();
+
+    @Before
+    public void setUp() {
+        input1.clear();
+        input1.add("apple");
+        input1.add("banana");
+        input1.add("pineapple");
+        input2.clear();
+        input2.add("cat");
+        input2.add("dog");
+        input2.add("rat");
+    }
+
+    @Test
+    public void testFilter() {
+        List<String> expected = new ArrayList<>();
+        expected.add("apple");
+        expected.add("pineapple");
+        StringChecker containsAP = s -> s.contains("ap");
+        assertArrayEquals(expected.toArray(), (ListExamples.filter(input1, containsAP)).toArray());
+    }
+
+    @Test
+    public void testMerge() {
+        List<String> expected = new ArrayList<>();
+        expected.add("apple");
+        expected.add("banana");
+        expected.add("cat");
+        expected.add("dog");
+        expected.add("pineapple");
+        expected.add("rat");
+        assertArrayEquals(expected.toArray(), (ListExamples.merge(input1, input2)).toArray());
+    }
 }
--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,9 +8,38 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -f student-submission/ListExamples.java ]]
+then
+    echo "ListExamples.java found"
+else
+    echo "ListExamples.java not found!"
+    exit 1
+fi
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+cp -r lib grading-area
+cp student-submission/ListExamples.java grading-area/
+cp TestListExamples.java grading-area/
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+cd grading-area
+javac -cp $CPATH *.java
+
+if [[ $? -ne 0 ]] 
+then 
+    echo "Compile error!"
+    exit 1
+else    
+    echo "Compiled successfully"
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+lastLine=$(tail -n 2 junit-output.txt | head -n 1)
+tests=$(echo $lastLine | awk -F'[, ]' '{print $3}')
+failures=$(echo $lastLine | awk -F'[, ]' '{print $6}')
+
+if [[ $tests =~ ^[0-9]+$ ]]; then
+    successes=$((tests - failures))
+    echo "Score: $successes / $tests"
+else
+    echo "All tests passed!"
+fi
